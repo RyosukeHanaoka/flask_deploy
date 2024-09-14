ec8a34595a54ab9e0c8a8e8bd063b1a2ef630adc
RyosukeHanaoka <138783421+RyosukeHanaoka@users.noreply.github.com>
2024-09-14 22:18:16 +0900

    変更

--- apps/data/models.py
+++ apps/data/models.py
@@ -3,19 +3,24 @@ from apps.data.extensions import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 from werkzeug.security import generate_password_hash, check_password_hash
+import random
 
 
 class User(UserMixin, db.Model):
     __tablename__ = "user"    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=func.now())
     updated_at = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
-    
-    def __init__(self, email, password):
+    verification_code = db.Column(db.String(6), nullable=True)
+    is_verified = db.Column(db.Boolean, default=False)
+
+    def __init__(self, email, password_hash,verification_code=None, is_verified=False):
         self.email = email
-        self.set_password(password)
+        self.password_hash = password_hash
+        self.verification_code = verification_code
+        self.is_verified = is_verified
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
